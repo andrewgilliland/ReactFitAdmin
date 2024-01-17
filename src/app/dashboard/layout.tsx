6e29e18d9cd42243b8425e19763a4061fbcdf251
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-01-17 14:45:56 -0600

    feat: adds tabs layout for dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,24 +15,22 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
     { name: "programs" },
   ];
 
-  useEffect(() => {
-    console.log("pathname: ", pathname);
-  }, [pathname]);
-
   return (
     <div className="px-10 py-12">
       <div className="flex gap-2">
         {routes.map(({ name }, index) => (
           <Link
             key={index}
-            className={`capitalize text-pink-500 border border-pink-500 px-4 py-2 rounded-md transition hover:bg-pink-500 hover:text-black`}
+            className={`capitalize font-semibold text-pink-500 border-t-2 border-x-2 border-pink-500 px-4 py-2 rounded-t-md transition hover:bg-pink-500 hover:text-black ${
+              pathname.includes(name) ? "bg-pink-500 text-black" : ""
+            }`}
             href={`/dashboard/${name}`}
           >
             {name}
           </Link>
         ))}
       </div>
-      <div className="border-2 border-pink-400 rounded mt-4 px-12 py-10">
+      <div className="border-2 border-pink-500 rounded-tr rounded-b px-12 py-10">
         {children}
       </div>
     </div>
