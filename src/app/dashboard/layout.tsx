f988dba1eddd9eed6a8baa075598e0221348eb4e
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-01-20 13:01:58 -0600

    fix: fixes layout tab bug

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -18,17 +18,23 @@ const DashboardLayout: FC<DashboardLayoutProps> = ({ children }) => {
   return (
     <div className="px-10 py-12">
       <div className="flex gap-2">
-        {routes.map(({ name }, index) => (
-          <Link
-            key={index}
-            className={`capitalize font-semibold text-pink-500 border-t-2 border-x-2 border-pink-500 px-4 py-2 rounded-t-md transition hover:bg-pink-500 hover:text-black ${
-              pathname.includes(name) ? "bg-pink-500 text-black" : ""
-            }`}
-            href={`/dashboard/${name}`}
-          >
-            {name}
-          </Link>
-        ))}
+        {routes.map(({ name }, index) => {
+          console.log(pathname, name, pathname.includes(name));
+
+          return (
+            <Link
+              key={index}
+              className={`capitalize font-semibold  border-t-2 border-x-2 border-pink-500 px-4 py-2 rounded-t-md transition hover:bg-pink-500 hover:text-black ${
+                pathname.includes(name)
+                  ? "bg-pink-500 text-black"
+                  : "bg-black text-pink-500"
+              }`}
+              href={`/dashboard/${name}`}
+            >
+              {name}
+            </Link>
+          );
+        })}
       </div>
       <div className="border-2 border-pink-500 rounded-tr rounded-b px-12 py-10">
         {children}
