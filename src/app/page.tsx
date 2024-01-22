af0ab5fb87297025837abb3077a08f65630cdc22
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-01-22 16:37:00 -0600

    feat: adds loginform component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 import Button from "@/components/Button";
 import Input from "@/components/Input";
+import LoginForm from "@/components/LoginForm";
 import Link from "next/link";
 
 const Home = () => (
   <main className="flex flex-col">
     <div className="flex flex-row justify-center">
-      <form
-        className="flex flex-col border-2 border-pink-400 rounded p-6"
-        action=""
-      >
-        <Input name="username" value="Biff" onChange={() => {}} />
-        <label htmlFor="">Password</label>
-        <input type="text" name="" id="" />
-        <Link href="/dashboard">
-          <Button>Login</Button>
-        </Link>
-      </form>
+      <LoginForm />
     </div>
   </main>
 );
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import Link from "next/link";
+import Input from "./Input";
+import Button from "./Button";
+
+const LoginForm = () => {
+  return (
+    <form
+      className="flex flex-col border-2 border-pink-400 rounded p-6"
+      action=""
+    >
+      <Input name="username" value="Biff" onChange={() => {}} />
+      <Input name="password" value="password" onChange={() => {}} />
+      <Link className="mt-3" href="/dashboard">
+        <Button>Login</Button>
+      </Link>
+    </form>
+  );
+};
+
+export default LoginForm;
