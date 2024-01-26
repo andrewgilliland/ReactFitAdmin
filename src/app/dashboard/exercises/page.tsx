b37d69d4a9019cd39cf4c7ab3aff69175f5d95ab
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-01-26 10:04:03 -0600

    fix: updates exercisecard min width

--- src/app/dashboard/exercises/page.tsx
+++ src/app/dashboard/exercises/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import ExerciseCard from "@/components/ExerciseCard";
 import { Exercise } from "@/types/Exercise";
-import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Exercises = () => {
@@ -19,17 +18,9 @@ const Exercises = () => {
 
   return (
     <div className="grid min-h-screen place-items-center">
-      <div className="flex flex-wrap justify-center w-full max-w-6xl border border-red-500 gap-4">
-        {exercises.slice(0, 4).map((exercise, index) => (
-          // <Link
-          //   href={`/dashboard/exercises/${exercise.id}`}
-          //   as={`/dashboard/exercises/${exercise.id}`}
-          //   key={`${exercise.name}-${index}`}
-          // >
-
+      <div className="flex flex-wrap justify-center w-full max-w-6xl border border-red-500 gap-6">
+        {exercises.map((exercise, index) => (
           <ExerciseCard key={`${exercise.name}-${index}`} exercise={exercise} />
-
-          // </Link>
         ))}
       </div>
     </div>
--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,4 +1,5 @@
 import { Exercise } from "@/types";
+import Link from "next/link";
 import { FC } from "react";
 
 type ExerciseCardProps = {
@@ -20,23 +21,30 @@ const ExerciseCard: FC<ExerciseCardProps> = ({ exercise, onClick }) => {
   return (
     <div
       onClick={() => onClick && onClick()}
-      className="relative flex flex-col h-80 bg-black border-2 border-cyan-500 rounded-lg group flex-1 hover:grow-[1.2] transition-all duration-500"
+      className="relative flex flex-col h-80 bg-black border-2 border-cyan-500 rounded-lg group flex-1 hover:grow-[1.2] transition-all duration-500 min-w-48"
     >
-      <div className="absolute inset-x-0 bottom-0 flex flex-col items-starts justify-start flex-1 max-w-48 p-6">
-        <h2 className="text-xl text-pink-500 font-bold leading-tight">
-          {name}
-        </h2>
-        <div className="grid grid-rows-[0fr] group-hover:grid-rows-[1fr] transition-all duration-500">
-          <div className="overflow-hidden mt-2 opacity-0 group-hover:opacity-100 transition duration-700">
-            <p className="text-gray-600">{difficulty}</p>
-            <p className="text-gray-600">{equipment}</p>
-            <p className="text-gray-600">{exerciseType}</p>
-            <p className="text-gray-600">{forceType}</p>
-            <p className="text-gray-600">{mechanics}</p>
-            <p className="text-gray-600">{targetMuscleGroup}</p>
+      <Link
+        className="border border-purple-500 h-full"
+        href={`/dashboard/exercises/${exercise.id}`}
+        as={`/dashboard/exercises/${exercise.id}`}
+        key={`${exercise.name}-${1}`}
+      >
+        <div className="absolute inset-x-0 bottom-0 flex flex-col items-starts justify-start flex-1 max-w-48 p-6">
+          <h2 className="text-xl text-pink-500 font-bold leading-tight">
+            {name}
+          </h2>
+          <div className="grid grid-rows-[0fr] group-hover:grid-rows-[1fr] transition-all duration-500">
+            <div className="overflow-hidden mt-2 opacity-0 group-hover:opacity-100 transition duration-700">
+              <p className="text-gray-600">{difficulty}</p>
+              <p className="text-gray-600">{equipment}</p>
+              <p className="text-gray-600">{exerciseType}</p>
+              <p className="text-gray-600">{forceType}</p>
+              <p className="text-gray-600">{mechanics}</p>
+              <p className="text-gray-600">{targetMuscleGroup}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
