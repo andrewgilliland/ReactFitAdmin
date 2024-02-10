044095bc35a7c4f82fdd877e2fb50a8694ea0404
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-02-10 07:49:26 -0600

    feat: wip createworkoutform exercises section

--- src/components/forms/CreateWorkoutForm.tsx
+++ src/components/forms/CreateWorkoutForm.tsx
@@ -2,6 +2,7 @@ import { Exercise, difficulty } from "@/types";
 import Input from "../Input";
 import Select from "../Select";
 import { FC } from "react";
+import Button from "../Button";
 
 type CreateExerciseFormProps = {
   exercises: Exercise[];
@@ -22,31 +23,36 @@ const CreateWorkoutForm: FC<CreateExerciseFormProps> = ({ exercises }) => {
       <div className="mt-6">
         <div className="text-sm text-gray-500">Exercises</div>
         <div className="border-2 border-pink-400 rounded mt-1 p-4">
-          <label className={`flex flex-col capitalize text-sm text-gray-500`}>
-            {"Exercise"}
-            <select
-              className={`bg-black border-2 border-pink-400 text-white rounded mt-1 px-2 py-1`}
-              name="exercise-1"
-            >
-              {exercises.map(({ id, name }) => (
-                <option className="capitalize" key={id} value={id}>
-                  {name}
-                </option>
-              ))}
-            </select>
-          </label>
-          <div className="mt-4">
-            <div className="text-sm text-gray-500">Sets</div>
-            <div className="border-2 border-pink-400 rounded mt-1 p-4">
-              <div className="text-sm text-gray-500">Set 1</div>
-              <Input
-                value={10}
-                name="repetitions"
-                type="number"
-                className="mt-1"
-              />
+          <div className="text-sm text-gray-500">Exercise 1</div>
+          <div className="border-2 border-yellow-400 rounded mt-1 p-4">
+            <label className={`flex flex-col capitalize text-sm text-gray-500`}>
+              {"Exercise"}
+              <select
+                className={`bg-black border-2 border-pink-400 text-white rounded mt-1 px-2 py-1`}
+                name="exercise-1"
+              >
+                {exercises.map(({ id, name }) => (
+                  <option className="capitalize" key={id} value={id}>
+                    {name}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <div className="mt-4">
+              <div className="text-sm text-gray-500">Sets</div>
+              <div className="border-2 border-pink-400 rounded mt-1 p-4">
+                <div className="text-sm text-gray-500">Set 1</div>
+                <Input
+                  value={10}
+                  name="repetitions"
+                  type="number"
+                  className="mt-1"
+                />
+                <Button className="mt-4">Add Set</Button>
+              </div>
             </div>
           </div>
+          <Button className="mt-4">Add Exercise</Button>
         </div>
       </div>
     </form>
