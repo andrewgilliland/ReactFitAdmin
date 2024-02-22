05386eaf1874eef471b19a5b5a944f1e878fa71f
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-02-22 17:05:40 -0600

    feat: updates workouts server action for supersets

--- src/components/forms/SupersetSetInput.tsx
+++ src/components/forms/SupersetSetInput.tsx
@@ -42,50 +42,54 @@ const SupersetSetInput: FC<SupersetSetInputProps> = ({
             const setIdentifier = `${setTypeIdentifier}-${setType[index]}`;
 
             return (
-              <div className={`flex mt-4`} key={`${name}-${index}`}>
+              <div className={`mt-4`} key={`${name}-${index}`}>
                 <div className="text-sm text-gray-500">
                   {exerciseNames[index] || `Exercise ${index + 1}`}
                 </div>
-                <fieldset className="mx-4">
-                  <legend className="text-sm text-gray-500">Set Type</legend>
-                  <div className="bg-pink-400 text-black font-semibold border border-pink-400 rounded-md max-w-min mt-1 p-1">
-                    {setTypes.map((type, i) => (
-                      <label
-                        key={type}
-                        className={`text-sm text-grey-500 px-2 py-1 transition-all 
+                <div className="flex mt-2">
+                  <fieldset className="mx-4">
+                    <legend className="text-sm text-gray-500">Set Type</legend>
+                    <div className="bg-pink-400 text-black font-semibold border border-pink-400 rounded-md max-w-min mt-1 p-1">
+                      {setTypes.map((type, i) => (
+                        <label
+                          key={type}
+                          className={`text-sm text-grey-500 px-2 py-1 transition-all 
                           ${
                             setType[index] === type
                               ? "bg-black text-pink-400"
                               : ""
                           }
                     ${i ? "rounded-r-md" : "rounded-l-md"}`}
-                      >
-                        <span>{`${capitalize(type)}`}</span>
-                        <input
-                          className="hidden"
-                          type="radio"
-                          name={setTypeIdentifier}
-                          value={type}
-                          checked={setType[index] === type}
-                          onChange={() =>
-                            setSetType((prev) => {
-                              const newSetType: [SetType, SetType] = [...prev];
-                              newSetType[index] = type;
-                              return newSetType;
-                            })
-                          }
-                        />
-                      </label>
-                    ))}
-                  </div>
-                </fieldset>
-                <Input
-                  value={setValue}
-                  label={setType[index]}
-                  name={setIdentifier}
-                  type="number"
-                  className="mt-1"
-                />
+                        >
+                          <span>{`${capitalize(type)}`}</span>
+                          <input
+                            className="hidden"
+                            type="radio"
+                            name={setTypeIdentifier}
+                            value={type}
+                            checked={setType[index] === type}
+                            onChange={() =>
+                              setSetType((prev) => {
+                                const newSetType: [SetType, SetType] = [
+                                  ...prev,
+                                ];
+                                newSetType[index] = type;
+                                return newSetType;
+                              })
+                            }
+                          />
+                        </label>
+                      ))}
+                    </div>
+                  </fieldset>
+                  <Input
+                    value={setValue}
+                    label={setType[index]}
+                    name={setIdentifier}
+                    type="number"
+                    className="mt-1"
+                  />
+                </div>
               </div>
             );
           })}
--- src/lib/actions/workouts.ts
+++ src/lib/actions/workouts.ts
@@ -33,9 +33,15 @@ const createWorkout = async (formData: FormData) => {
 
   console.log("newWorkout: ", newWorkout);
 
-  newWorkout.exercises.forEach((exercise) => {
-    console.log("exercise: ", exercise.sets);
-  });
+  // newWorkout.exercises.forEach((exercise) => {
+  //   if (Array.isArray(exercise)) {
+  //     exercise.forEach((subExercise) => {
+  //       console.log("subExercise: ", subExercise);
+  //     });
+  //   } else {
+  //     console.log("exercise: ", exercise);
+  //   }
+  // });
 };
 
 const updateWorkout = async (formData: FormData) => {
@@ -45,16 +51,76 @@ const updateWorkout = async (formData: FormData) => {
 const formatExercises = (formData: FormData) => {
   const exercises = [];
   let exerciseIndex = 1;
+  let supersetIndex = 1;
+  let supersetExerciseIndex = 1;
 
-  while (formData.get(`exercise-${exerciseIndex}`)) {
-    exercises.push({
-      id: formData.get(`exercise-${exerciseIndex}`),
-      sets: formatSets(formData, exerciseIndex),
-    });
-    exerciseIndex++;
+  while (
+    formData.get(`superset-${supersetIndex}-exercise-${supersetExerciseIndex}`)
+  ) {
+    const superset = [];
+    while (
+      formData.get(
+        `superset-${supersetIndex}-exercise-${supersetExerciseIndex}`
+      )
+    ) {
+      superset.push({
+        id: formData.get(
+          `superset-${supersetIndex}-exercise-${supersetExerciseIndex}`
+        ),
+        sets: formatSupersetSets(
+          formData,
+          supersetIndex,
+          supersetExerciseIndex
+        ),
+      });
+
+      supersetExerciseIndex++;
+    }
+    supersetIndex++;
+    supersetExerciseIndex = 1;
+    exercises.push(superset);
   }
 
+  while (formData.get(`exercise-${supersetIndex}`)) {
+    exercises.push({
+      id: formData.get(`exercise-${supersetIndex}`),
+      sets: formatSets(formData, supersetIndex),
+    });
+    supersetIndex++;
+  }
+  console.log("exerciseIndex: ", exerciseIndex);
+  console.log("supersetIndex: ", supersetIndex);
+  console.log("supersetExerciseIndex: ", supersetExerciseIndex);
+
   return exercises;
+};
+
+const formatSupersetSets = (
+  formData: FormData,
+  supersetIndex: number,
+  exerciseIndex: number
+) => {
+  const sets = [];
+  let setIndex = 1;
+
+  while (
+    formData.get(
+      `superset-${supersetIndex}-exercise-${exerciseIndex}-set-${setIndex}`
+    )
+  ) {
+    const setType = formData.get(
+      `superset-${supersetIndex}-exercise-${exerciseIndex}-set-${setIndex}`
+    ) as SetType;
+
+    sets.push({
+      [setType]: formData.get(
+        `superset-${supersetIndex}-exercise-${exerciseIndex}-set-${setIndex}-${setType}`
+      ),
+    });
+    setIndex++;
+  }
+
+  return sets;
 };
 
 const formatSets = (formData: FormData, exerciseIndex: number) => {
