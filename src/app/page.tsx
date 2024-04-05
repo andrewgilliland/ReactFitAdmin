2092963ec727966952f4ec57479c5debe005f821
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-04-05 17:04:23 -0500

    feat: wip sliderform component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
+"use client";
+import SliderForm from "@/components/SliderForm";
 import TypeWriterText from "@/components/TypeWriterText";
 import SignUpForm from "@/components/forms/auth/SignUpForm";
 
 const Home = () => (
   <main className="flex flex-col">
     <div className="border-2 border-pink-400 rounded p-10">
-      <div className="bg-gray-900 h-96 w-"> </div>
-      <p className="font-semibold text-3xl">
+      <SliderForm />
+      {/* <p className="font-semibold text-3xl mt-4">
         I want to be{" "}
         <TypeWriterText
           words={["fitter", "stronger", "healthier", "happier", "better"]}
         />
-      </p>
+      </p> */}
     </div>
   </main>
 );
